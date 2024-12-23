bdf9d9a528ab5d4a600fedfa3cb28f827cfa4c48
LucasGawronek <106654205+LucasGawronek@users.noreply.github.com>
2024-12-23 14:18:50 +0100

    FIles
    Files

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+const convertBtn = document.getElementById("convert-btn");
+const meterEl = document.getElementById("meter");
+const volumeEl = document.getElementById("volume");
+const massEl = document.getElementById("mass");
+
+convertBtn.addEventListener("click", function () {
+  console.log("clicked");
+});
