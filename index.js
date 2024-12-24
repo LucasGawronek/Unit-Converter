c1151a3fb6b9193ae204b6847fd41132e90a1317
LucasGawronek <106654205+LucasGawronek@users.noreply.github.com>
2024-12-24 14:59:10 +0100

    button function

--- index.js
+++ index.js
@@ -3,12 +3,20 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
-
+let userInput = "";
+const meter = 3.281;
+const liter = 0.264;
+const kilogram = 2.204;
 const convertBtn = document.getElementById("convert-btn");
-const meterEl = document.getElementById("meter");
-const volumeEl = document.getElementById("volume");
-const massEl = document.getElementById("mass");
+const unitInput = document.getElementById("unitInput");
 
 convertBtn.addEventListener("click", function () {
-  console.log("clicked");
+  userInput = parseFloat(unitInput.value);
+  if (isNaN(userInput)) {
+    alert("Not a number");
+  } else {
+    console.log(userInput);
+  }
 });
+
+function outputLength() {}
