010aaf1743f1818b77da06591b58f57601258939
LucasGawronek <106654205+LucasGawronek@users.noreply.github.com>
2024-12-25 19:52:48 +0100

    added more functions

--- index.js
+++ index.js
@@ -4,19 +4,35 @@
 1 kilogram = 2.204 pound
 */
 let userInput = "";
-const meter = 3.281;
-const liter = 0.264;
-const kilogram = 2.204;
+const units = {
+  feet: 3.280839895,
+  liter: 0.264,
+  kilogram: 2.204,
+  meter: 0.3048,
+};
 const convertBtn = document.getElementById("convert-btn");
 const unitInput = document.getElementById("unitInput");
+let ulEl = document.getElementById("unit-list");
+let lengthEl = document.getElementById("length-el");
+let volumeEl = document.getElementById("volume-el");
+let massEl = document.getElementById("mass-el");
 
 convertBtn.addEventListener("click", function () {
   userInput = parseFloat(unitInput.value);
   if (isNaN(userInput)) {
     alert("Not a number");
   } else {
-    console.log(userInput);
+    clearList();
+    outputLength();
   }
 });
 
-function outputLength() {}
+function outputLength() {
+  mt2Ft = userInput * units.feet.toFixed(4);
+  ft2mt = userInput * units.meter.toFixed(4);
+  lengthEl.innerHTML += `<p>${userInput} meters = ${mt2Ft} feet | ${userInput} feet = ${ft2mt} meters</p>`;
+}
+
+function clearList() {
+  lengthEl.removeChild(lengthEl.firstElementChild);
+}
